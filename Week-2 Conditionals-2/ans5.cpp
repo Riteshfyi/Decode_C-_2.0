9eb570c48c4741e9f31247fa260313f00421e310
Ritesh <riteshfyi@gmail.com>
2024-09-15 00:50:24 +0530

    updates

--- Week-2 Conditionals-2/ans5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int a = 18; int b = 12;
-    bool t = (a > 20 && b < 15)? true : false;
-    cout <<"Value of t: " << t ;
-    return 0;
-}
--- week-1 asg1/ans1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Physics"<< endl ;
-    cout << "Wallah"<< endl ;
-    return 0;
-}
